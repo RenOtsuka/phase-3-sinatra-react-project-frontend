8a99e002e561a259e8420fb1819f43aa240f94c9
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-07-13 12:29:50 -0500

    Add form to create new toDo

---
+++ sinatra-react-project-frontend/src/components/NewItem.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function NewItem({addItem}){
+
+    const [taskName, setTaskName] = useState("");
+    const [itemCategory, setCategory] = useState("");
+
+    function handleSubmit(e){
+        e.preventDefault();
+        fetch(`http://localhost:9292/todos`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                task: taskName,
+                category : itemCategory,
+                completed: "false",
+            })
+        })
+        .then(r => r.json())
+        .then(data => addItem(data))
+        .catch(error => alert(error));
+        
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Enter task desciption..." value={taskName} onChange={(e) => setTaskName(e.target.value)}/>         
+            <input type="text" placeholder="Enter category..." value={category} onChange={(e) => setCategory(e.target.value)}/>  
+            <button type="submit">Submit</button>          
+        </form>
+    )
+}
+
+export default NewItem;
