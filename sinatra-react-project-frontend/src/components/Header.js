2a6a9d1dbdaec61c59f941fe95b56b40f8a6ff69
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-07-13 12:25:20 -0500

    Add components folder

---
+++ sinatra-react-project-frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header(){
+    return (
+        <header>
+            <br/>
+            <h1>To Do List</h1>
+            <div className="SubHeader">
+                <h3>Task</h3>
+                <h3>Completed</h3>
+                <h3>Category</h3>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
