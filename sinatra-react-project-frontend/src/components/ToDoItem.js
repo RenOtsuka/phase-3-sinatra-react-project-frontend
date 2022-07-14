189184a1e9576f8d6990b0aa21204fb6c4aca61b
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-07-14 12:52:04 -0500

    Add editing functionality to ToDoItem

--- sinatra-react-project-frontend/src/components/ToDoItem.js
+++ sinatra-react-project-frontend/src/components/ToDoItem.js
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
+import EditItem from "./EditItem";
 
-function ToDoItem({id, task, category, completed, onDeleteItem}){
+function ToDoItem({id, task, category, completed, onDeleteItem, onEditItem}){
+
+    const [editToggle, setEditToggle] = useState(false);
     
     function deleteItem(){
         fetch(`http://localhost:9292/todos/${id}`, {
@@ -10,12 +13,29 @@ function ToDoItem({id, task, category, completed, onDeleteItem}){
         onDeleteItem(id);
     }
 
+    function editItem(editedItem){
+        setEditToggle(false);
+        onEditItem(editedItem);
+    }   
+
     return(
         <div className="ListItem">
-            <p>{task}</p>
-            <p>{category}</p>
-            <p>{completed}</p>
-            <button onClick={deleteItem}/>
+            {editToggle ? (
+            <EditItem 
+                id={id} 
+                task={task} 
+                category={category} 
+                completed={completed}
+                editItem={editItem}/>
+            ) : (
+                <div className="body">
+                    <p>{task}</p>
+                    <p>{category}</p>
+                    <p>{completed}</p>
+                </div>
+            )}
+            <button onClick={editItem}>Edit</button>
+            <button onClick={deleteItem}>Delete</button>
         </div>
     )
 }
