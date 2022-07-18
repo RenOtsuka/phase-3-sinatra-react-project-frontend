aab4fe28e683c112160a6aaca3562aa6ba272b9e
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-07-18 14:14:51 -0500

    update props to pass

--- sinatra-react-project-frontend/src/components/ToDoList.js
+++ sinatra-react-project-frontend/src/components/ToDoList.js
@@ -2,17 +2,20 @@ import React from "react";
 import ToDoItem from "./ToDoItem";
 
 function ToDoList({list, onDeleteItem, onEditItem}){
-    toDoList = list.map( item => (
+    const toDoList = list.map( item => (
         <ToDoItem
         key={item.id}
         id={item.id}
         task={item.task}
-        category_id={item.category_id}
+        category={item.category_name}
         completed={item.completed}
         onDeleteItem={onDeleteItem}
         onEditItem={onEditItem}
         />
     ))
+    return (
+        <ul className="toDoList">{toDoList}</ul>
+    )
 }
 
 export default ToDoList;
