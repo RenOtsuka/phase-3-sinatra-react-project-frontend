c088a3bb9faa5ae7fc8aa7f7e905c58d20a28174
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-07-18 14:13:23 -0500

    Add ToDoListDisplay to app

--- sinatra-react-project-frontend/src/components/App.js
+++ sinatra-react-project-frontend/src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import './App.css'
 import Header from "./Header";
+import ToDoListDisplay from "./ToDoListDisplay";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <ToDoListDisplay/>
     </div>
   );
 }
