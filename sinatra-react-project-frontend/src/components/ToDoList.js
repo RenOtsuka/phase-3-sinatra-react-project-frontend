3bbcf52d8772fba7828b8122c8c6ebdef60eeff7
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-08-11 15:31:41 -0500

    Add category obj to pass as props

--- sinatra-react-project-frontend/src/components/ToDoList.js
+++ sinatra-react-project-frontend/src/components/ToDoList.js
@@ -2,13 +2,16 @@ import React from "react";
 import ToDoItem from "./ToDoItem";
 
 function ToDoList({list, onDeleteItem, onEditItem}){
+
+    // console.log(list)
+
     const toDoList = list.map( item => (
         <ToDoItem
         key={item.id}
         id={item.id}
         task={item.task}
-        category={item.category_name}
-        completed={item.completed}
+        category_id={item.category_id}
+        categoryObj={item.category}
         onDeleteItem={onDeleteItem}
         onEditItem={onEditItem}
         />
