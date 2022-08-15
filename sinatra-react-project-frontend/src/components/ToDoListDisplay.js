2365e9e3c50cca470aafd815e9d3eaa66b0c4288
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-08-15 17:58:58 -0500

    Moved Header comp into this file

--- sinatra-react-project-frontend/src/components/ToDoListDisplay.js
+++ sinatra-react-project-frontend/src/components/ToDoListDisplay.js
@@ -1,26 +1,18 @@
 import React, {useState, useEffect} from "react";
 import ToDoList from "./ToDoList";
 import NewItem from "./NewItem";
+import Header from "./Header";
 
-function ToDoListDisplay(){
+function ToDoListDisplay({categoryList}){
     const [toDoList, setToDoList] = useState([]);
-    const [categoryList, setCategoryList] = useState([]);
 
     useEffect(() => {
         fetch("http://localhost:9292/todos")
         .then((r) => r.json())
         .then((data) => setToDoList(data))
         .catch(error => alert(error));
-
        
     },[]);
-
-    useEffect(() => {
-        fetch(`http://localhost:9292/categories`)
-        .then((r) => r.json())
-        .then((data) => setCategoryList(data))
-        .catch(error => alert(error));
-    },[])
 
     function addItem(itemObj){
         setToDoList([...toDoList, itemObj]);
@@ -44,10 +36,9 @@ function ToDoListDisplay(){
         setToDoList(updateList);
     }
 
-    // console.log(categoryList[0].id)
-
     return(
         <div className="ToDoListDisplay">
+            <Header/>
             <NewItem addItem={addItem} categoryList={categoryList}/>
             <br/>
             <ToDoList list={toDoList} onDeleteItem={onDeleteItem} onEditItem={onEditItem}/>
