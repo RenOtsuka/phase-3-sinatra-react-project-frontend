7235e26f17103430b0253722c42d2fc542546efe
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-08-22 12:54:04 -0500

    Add fetch of todos

--- sinatra-react-project-frontend/src/components/App.js
+++ sinatra-react-project-frontend/src/components/App.js
@@ -1,13 +1,44 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 import './App.css'
-import Header from "./Header";
+// import {Route, Switch} from "react-router-dom";
+import NavBar from "./NavBar";
 import ToDoListDisplay from "./ToDoListDisplay";
+import CategoryPage from "./CategoryPage";
+
 
 function App() {
+
+  const [categoryList, setCategoryList] = useState([]);
+  const [toDoList, setToDoList] = useState([]);
+
+
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/categories`)
+    .then((r) => r.json())
+    .then((data) => setCategoryList(data))
+    .catch(error => alert(error));
+  },[]);
+
+
+  useEffect(() => {
+        fetch("http://localhost:9292/todos")
+        .then((r) => r.json())
+        .then((data) => setToDoList(data))
+        .catch(error => alert(error));
+  },[]);
+
+
   return (
     <div className="App">
-      <Header/>
-      <ToDoListDisplay/>
+      {/* <NavBar/> 
+      <Switch>
+        <Route exact path="/todos"><ToDoListDisplay categoryList={categoryList}/></Route>
+        <Route exact path="/categories"><CategoryPage categoryList={categoryList} setCategoryList={setCategoryList}/></Route>
+      </Switch>      */}
+      <ToDoListDisplay categoryList={categoryList} toDoList={toDoList} setToDoList={setToDoList}/>
+      <CategoryPage categoryList={categoryList} setCategoryList={setCategoryList} toDoList={toDoList}/>
+
     </div>
   );
 }
