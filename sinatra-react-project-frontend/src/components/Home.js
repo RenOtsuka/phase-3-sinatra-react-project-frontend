94ed1a6f3ffecb3473ef2609ad4df1b711aff62b
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-08-26 11:46:54 -0500

    Add Home component

---
+++ sinatra-react-project-frontend/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="Home">
+            <br/>
+            <h1>Home Page</h1>
+            <h2>Welcome to my React/Ruby Project</h2>
+            <br/>
+           <img src="https://www.freeiconspng.com/uploads/checklist-icon-checklist-icon-png-list-icon-7.png" width="350" alt="Homepage" />
+        </div>
+    );
+}
+
+export default Home;
